c08f8da27178659300b21205e15b19abd533834b
Macro <fuhong.zhou@qq.com>
2022-06-26 06:52:54 +0800

    perfect test

--- DKCommunicationTEST/UnitTest1.cs
+++ DKCommunicationTEST/UnitTest1.cs
@@ -11,6 +11,12 @@ namespace DKCommunicationTEST
             var a = device.HandShake();
             var b = device.SetSystemMode(SystemMode.ModeStandardSourceCalibrate);
             var c= device.SetSystemMode(SystemMode.ModeStandardMeterCalibrate);
+            
+            Assert.True(a[a.Length-1].Equals(0x4b));
+            Assert.True(b[b.Length-1].Equals(0x46));
+            Assert.True(c[c.Length-1].Equals(0x47));
+
+            
         }
     }
 }
